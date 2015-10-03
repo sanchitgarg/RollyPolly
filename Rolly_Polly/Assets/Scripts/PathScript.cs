54880e240ee8424d9b6589287da444a6858c5919
Sanchit Garg <sanchit.garg0526@gmail.com>
2015-10-03 15:32:29 -0400

    Initial game setup with paths

---
+++ Rolly_Polly/Assets/Scripts/PathScript.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PathScript: MonoBehaviour {
+
+	public GameObject pathObject;
+	public float zSpeed;
+
+	// Use this for initialization
+	void Start () {
+		//zSpeed = -100;
+		var rb = GetComponent<Rigidbody> ();
+		rb.velocity = new Vector3 (0, 0, zSpeed);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+	}
+
+	void OnCollisionEnter(Collision collision)
+	{
+		Collider c = collision.collider;
+
+		if (c.CompareTag ("CreatePathColliderTag")) {
+			Vector3 position = new Vector3(0,0, 50);
+			Instantiate(pathObject, position, Quaternion.identity);
+		}
+
+		else if (c.CompareTag ("DestroyPathColliderTag")) {
+			Destroy(gameObject);
+		}
+	}
+}
